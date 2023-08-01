50d657bda7d729b69b19399c5b73c39c2bb2a209
ByteOtter <christopher.hock@suse.com>
2023-08-01 11:48:52 +0200

    remove leftover code from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod collectors;
 
-use collectors::{dmi_collector, network_collector};
-
+use collectors::dmi_collector;
 
 fn main() {
-    let output = dmi_collector::dmidecode();
+    let output: dmi_collector::DmiInformation = dmi_collector::dmidecode();
     println!("{:?}", output);
 }
