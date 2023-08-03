3cc79c12e51c9fd86a92855612b93ad834edba41
ByteOtter <christopher.hock@suse.com>
2023-08-03 15:09:01 +0200

    Add collection of network interface speed

--- src/collectors/network_collector.rs
+++ src/collectors/network_collector.rs
@@ -2,15 +2,11 @@
 //!
 //! This module provides logic to collect and process Information about all network interfaces a device has.
 //!
-
-/*
-TODO:
-- Implement Error Checking
-*/
 use network_interface::Addr::{V4, V6};
 use network_interface::{NetworkInterface, NetworkInterfaceConfig};
 use std::fs;
 use std::net::IpAddr;
+use std::process::{Command, Output};
 
 use super::collector_exceptions;
 
@@ -31,9 +27,11 @@ use super::collector_exceptions;
 /// * index - `u32` the index of this interface.
 /// * is_physical - `bool` whether this device is a physical or virtual interface.
 /// * is_connected - `bool` whether the interface is connected. Determined by if it has an address or not.
+/// * interface_speed - `u32` the speed of the interface.
 #[derive(Debug)]
 pub struct NetworkInformation {
     name: String,
+    interface_speed: Option<u32>,
     v4ip: Option<IpAddr>,
     v4broadcast: Option<IpAddr>,
     v4netmask: Option<IpAddr>,
@@ -41,7 +39,7 @@ pub struct NetworkInformation {
     v6broadcast: Option<IpAddr>,
     v6netmask: Option<IpAddr>,
     mac_addr: Option<String>,
-    index: u32,
+    index: Option<u32>,
     is_physical: bool,
     is_connected: bool,
 }
@@ -118,7 +116,7 @@ pub fn construct_network_information(
                 // Cases where only one set of addresses exist.
                 if network_interface.addr.is_empty() {
                     network_information = NetworkInformation {
-                        name: network_interface.name,
+                        name: network_interface.name.clone(),
                         v4ip: None,
                         v4broadcast: None,
                         v4netmask: None,
@@ -126,16 +124,20 @@ pub fn construct_network_information(
                         v6broadcast: None,
                         v6netmask: None,
                         mac_addr: network_interface.mac_addr,
-                        index: network_interface.index,
+                        index: Some(network_interface.index),
                         is_physical: true,
                         is_connected: false,
+                        interface_speed: match collect_interface_speed(&network_interface.name) {
+                            Ok(speed) => Some(speed),
+                            Err(_) => None,
+                        },
                     };
                 } else if network_interface.addr.len() == 1 {
                     // Match if the only set of ip addresses is ipv4 or ipv6
                     match network_interface.addr[0] {
                         V4(_ip) => {
                             network_information = NetworkInformation {
-                                name: network_interface.name,
+                                name: network_interface.name.clone(),
                                 v4ip: Some(network_interface.addr[0].ip()),
                                 v4broadcast: network_interface.addr[0].broadcast(),
                                 v4netmask: network_interface.addr[0].netmask(),
@@ -143,14 +145,20 @@ pub fn construct_network_information(
                                 v6broadcast: None,
                                 v6netmask: None,
                                 mac_addr: network_interface.mac_addr,
-                                index: network_interface.index,
+                                index: Some(network_interface.index),
                                 is_physical: true,
                                 is_connected: true,
+                                interface_speed: match collect_interface_speed(
+                                    &network_interface.name,
+                                ) {
+                                    Ok(speed) => Some(speed),
+                                    Err(_) => None,
+                                },
                             }
                         }
                         V6(_ip) => {
                             network_information = NetworkInformation {
-                                name: network_interface.name,
+                                name: network_interface.name.clone(),
                                 v4ip: None,
                                 v4broadcast: None,
                                 v4netmask: None,
@@ -158,15 +166,21 @@ pub fn construct_network_information(
                                 v6broadcast: network_interface.addr[0].broadcast(),
                                 v6netmask: network_interface.addr[0].netmask(),
                                 mac_addr: network_interface.mac_addr,
-                                index: network_interface.index,
+                                index: Some(network_interface.index),
                                 is_physical: true,
                                 is_connected: true,
+                                interface_speed: match collect_interface_speed(
+                                    &network_interface.name,
+                                ) {
+                                    Ok(speed) => Some(speed),
+                                    Err(_) => None,
+                                },
                             }
                         }
                     }
                 } else {
                     network_information = NetworkInformation {
-                        name: network_interface.name,
+                        name: network_interface.name.clone(),
                         v4ip: Some(network_interface.addr[0].ip()),
                         v4broadcast: network_interface.addr[0].broadcast(),
                         v4netmask: network_interface.addr[0].netmask(),
@@ -174,9 +188,13 @@ pub fn construct_network_information(
                         v6broadcast: network_interface.addr[1].broadcast(),
                         v6netmask: network_interface.addr[1].netmask(),
                         mac_addr: network_interface.mac_addr,
-                        index: network_interface.index,
+                        index: Some(network_interface.index),
                         is_physical: true,
                         is_connected: true,
+                        interface_speed: match collect_interface_speed(&network_interface.name) {
+                            Ok(speed) => Some(speed),
+                            Err(_) => None,
+                        },
                     };
                 }
             }
@@ -186,7 +204,6 @@ pub fn construct_network_information(
                 let exc = collector_exceptions::InvalidNetworkInterfaceError {
                     message: "FATAL: A Network interface cannot be recognized!".to_string(),
                 };
-
                 exc.panic();
             }
         }
@@ -209,7 +226,7 @@ pub fn construct_network_information(
 ///
 /// ### Arguments
 ///
-/// * `interface` - A string slice that holds the name of the network interface.
+/// * `interface_name` - A string slice that holds the name of the network interface.
 ///
 /// ### Returns
 ///
@@ -227,4 +244,53 @@ fn check_for_physical_nw(interface_name: &str) -> bool {
     let path: String = format!("/sys/class/net/{}/device/virtfn0", interface_name);
 
     return fs::metadata(&path).is_ok();
+}
+
+/// Will attempt to collect the network interface's speed by running `ethtool <interface_name> | grep "Speed"` and
+/// catching the output.
+///
+/// If "Speed" is to be reported as "Unknown!", None is returned.
+///
+/// ## Arguments
+///
+/// * `interface_name: &str` - The name of the interface to investigate.
+///
+/// ## Returns
+///
+/// * `Ok(u32)` - If the entry for interface speed returned by ethtool is not "Unknown".
+/// * `Err` - If the entry for interface speed returned by ethtool is "Unknown".
+fn collect_interface_speed(interface_name: &str) -> Result<u32, String> {
+    let output: Output = Command::new("ethtool")
+        .arg("-s")
+        .arg(interface_name)
+        .output()
+        .map_err(|e: std::io::Error| format!("Failed to execute ethtool command: {}", e))?;
+
+    if !output.status.success() {
+        return Err(format!(
+            "ethtool command failed with exit code: {}",
+            output.status
+        ));
+    }
+
+    let output_str: String = String::from_utf8_lossy(&output.stdout).to_string();
+
+    let speed_str: &str = output_str
+        .lines()
+        .find(|line: &&str| line.starts_with("Speed:"))
+        .and_then(|line: &str| line.split_whitespace().nth(1))
+        .ok_or_else(|| "Failed to parse speed from ethtool output".to_string())?;
+
+    if speed_str == "Unknown!" {
+        return Err(format!(
+            "INFO: No interface speed known for {}. The interface may be virtual or disabled.",
+            interface_name
+        ));
+    }
+
+    let interface_speed: u32 = speed_str
+        .parse::<u32>()
+        .map_err(|e: std::num::ParseIntError| format!("Failed to parse speed as u32: {}", e))?;
+
+    Ok(interface_speed)
 }
