ed598f049a696f81a7414a9138f9b9be4a05c272
ByteOtter <christopher.hock@suse.com>
2023-08-14 15:53:00 +0200

    Update pre-commit-hooks for code quality

--- .pre-commit-hooks/clippy.sh
+++ .pre-commit-hooks/clippy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-STAGED=$(git diff --name-only --cached | grep '.*\.rs')
-if ! [ "$STAGED" = '' ]; then
-    cargo clippy "$STAGED" || {
-        echo -e "\e[31mPlease run cargo clippy on all staged files before committing\e[0m" ; exit 1
-    }
+cargo clippy -- -Dwarnings 2> /dev/null
+clippy_exit_code=$?
+if [ $clippy_exit_code != 0 ]; then
+    echo -e "\e[31mYour code seems to contains style violations! Run cargo clippy before committing!\e[0m" ; exit 1
 fi
--- .pre-commit-hooks/rustfmt.sh
+++ .pre-commit-hooks/rustfmt.sh
@@ -2,6 +2,6 @@
 STAGED=$(git diff --name-only --cached | grep '.*\.rs')
 if ! [ "$STAGED" = '' ]; then
     rustfmt --check "$STAGED" || {
-        echo -e "\e[31mPlease run rusftmt on all staged files before committing\e[0m" ; exit 1
+        echo -e "\e[31mYour code is not formatted correctly! Please run rustfmt on all staged files before committing!\e[0m" ; exit 1
     }
 fi
