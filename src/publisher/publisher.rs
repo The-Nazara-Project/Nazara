5c95bc8a6b188ab592da393165967d490eb6eba9
ByteOtter <christopher-hock@protonmail.com>
2023-11-08 10:17:53 +0100

    Add proper documentation

--- src/publisher/publisher.rs
+++ src/publisher/publisher.rs
@@ -1,5 +1,8 @@
 //! This module contains logic for the HTTP NetBox API client.
-
+//!
+//! This client allows us to get a list of all machines and create or update machines or VMs.
+//! We use the `reqwest` crate for blocking HTTP calles and `serde` together with `serde_json` to serialize and
+//! deserialize our data.
 use reqwest::{blocking::Client, Error as ReqwestError};
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -9,6 +12,9 @@ use crate::collectors::{dmi_collector::DmiInformation, network_collector::Networ
 use super::publisher_exceptions;
 
 /// NetBox API client.
+///
+/// This client encapsulates the base URL and API token necessary for authenticating with the NetBox API
+/// as well as a `reqwest` client for making HTTP requests.
 ///
 /// # Members
 ///
@@ -21,7 +27,13 @@ pub struct NetBoxClient {
     client: Client,
 }
 
-/// System Information.
+/// Represents the combined system information required to create a new machine or update an existing one.
+///
+/// # Members
+///
+/// * `dmi_information: DmiInformation` - DMI (Desktop Management Interface) details of the system such as model and manufacturer.
+/// * `network_information: NetworkInformation` - A list of network interface details.
+/// * `system_location: String` A string representing the location or office the system is located at. Read from the config file.
 #[derive(Serialize)]
 pub struct SystemData {
     pub dmi_information: DmiInformation,
@@ -29,13 +41,32 @@ pub struct SystemData {
     pub system_location: String,
 }
 
-/// Request Payload for machine creation.
+/// Encapsulates the payload required to create a new machine in NetBox.
+///
+/// This struct is serialized into JSON format and sent as the body of the HTTP request to create a new machine.
+///
+/// # Members
+///
+/// * `system_information: SystemData` - System Information to include in the request.
 #[derive(Serialize)]
 pub struct CreateMachinePayload {
     pub system_information: SystemData,
 }
 
 impl NetBoxClient {
+    /// Constructs a new `NetBoxClient` with the given base URL and API token.
+    ///
+    /// This constructor initializes the internal reqwest client and stores the provided base URL or API token read
+    /// from the command line or configuration file.
+    ///
+    /// # Arguments
+    ///
+    /// * `base_url: &str` - A string slice that holds the base URL of the NetBox API.
+    /// * `api_token: &str` - A string slice that holds the API token for authenticating with the NetBox API
+    ///
+    /// # Returns
+    ///
+    /// A new instance of the `NetBoxClient`
     pub fn new(base_url: &str, api_token: &str) -> Self {
         let client: Client = Client::new();
         NetBoxClient {
@@ -45,7 +76,20 @@ impl NetBoxClient {
         }
     }
 
-    /// Create machine in NetBox.
+    /// Sends a request to the NetBox API to create a new machine.
+    ///
+    /// This method posts the provided payload to the machine creation endpoint of the API.
+    /// It handles the serialization of the payload to JSON format, sets the necessary HTTP headers for authentication,
+    /// and checks the API response to determine if the request was successful.
+    ///
+    /// # Arguments
+    ///
+    /// * `payload: &CreateMachinePayload` - A reference to a `CreateMachinePayload` instance containing all decessary payload information.
+    ///
+    /// # Returns
+    ///
+    /// Returns `Ok(())` if the machine was created successfully.
+    /// Returns an `Err` with `publisher_exceptions::NetBoxApiError` if the request failed.
     pub fn create_machine(
         &self,
         payload: &CreateMachinePayload,
