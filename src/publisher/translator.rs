2367b6c557eb69832876e90fa2966c47cb12a310
ByteOtter <christopher-hock@protonmail.com>
2024-03-07 08:17:15 +0100

    implement machine info to device translation

--- src/publisher/translator.rs
+++ src/publisher/translator.rs
@@ -12,11 +12,32 @@ use crate::{configuration::config_parser::ConfigData, Machine};
 
 /// Translate the machine information to a `WritableDeviceWithConfigContextRequest` required by
 /// NetBox's API.
+///
+/// # Parameters
+///
+/// - `machine: &Machine` - Collected information about the device
+/// - `config_data: ConfigData` - Additional information about the device provided by config file
+/// or CLI
+///
+/// # Returns
+///
+/// - `device: WritableDeviceWithConfigContextRequest` - Payload for machine creation request
 pub fn information_to_device(
     machine: &Machine,
     config_data: ConfigData,
 ) -> WritableDeviceWithConfigContextRequest {
+    //    let device: WritableDeviceWithConfigContextRequest = WritableDeviceWithConfigContextRequest {
+    //        name: Some(config_data.system.name),
+    //        device_type: config_data.system.device_type,
+    //        role: config_data.system.device_role,
+    //        tenant: config_data.system.tenant,
+    //    };
+    //    device
     todo!()
+}
+
+fn get_platform_id(machine: &Machine) -> Option<i64> {
+    todo!("Get platform by name an return platform id!")
 }
 
 pub fn information_to_vm(machine: &Machine) -> WritableVirtualMachineWithConfigContextRequest {
