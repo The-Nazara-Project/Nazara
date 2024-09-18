7d8aa302d7d777cf23f28120a8b2bada1ad27679
TiaraNivani <tiara.dev@proton.me>
2024-09-19 01:15:15 +0200

    Remove obsolete error structs

--- src/configuration/config_exceptions.rs
+++ src/configuration/config_exceptions.rs
@@ -10,7 +10,7 @@
 //!
 //! |Code  |Name                    |Explanation                                                |
 //! |------|------------------------|-----------------------------------------------------------|
-//! |`10`  |ConfigFileError         |Indicates errors with operating on the config file.        |
+//! |`10`  |FileOpError             |Indicates errors with operating on the config file.        |
 //! |`11`  |NoConfigFileErrror      |Configuration file could not be found.                     |
 //! |`12`  |InvalidConfigFileError  |Contains a TOML-Deserialize error.                         |
 //! |`13`  |SerializationError      |Constains a TOML-Serialization error.                      |
@@ -132,113 +132,5 @@ impl ConfigError {
             ConfigError::MissingConfigOptionError(_) => 14,
             ConfigError::Other(_) => 19,
         }
-    }
-}
-
-/// This error is raised when the program cannot create a config file.
-///
-/// As a config file is required for the program's operation, it must abort.
-pub struct UnableToCreateConfigError {
-    pub message: String,
-}
-
-impl fmt::Display for UnableToCreateConfigError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl UnableToCreateConfigError {
-    pub fn abort(&self, exit_code: i32) -> ! {
-        println!("{} (Error code: {})", self, exit_code);
-        process::exit(exit_code)
-    }
-}
-
-/// This exception is raised when the program cannot write to the config file.
-///
-/// E.g. when the user changes config parameters via the CLI.
-pub struct ConfigWriteException {
-    pub message: String,
-}
-
-impl fmt::Display for ConfigWriteException {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-/// This error is raised when the config fil cannot be read for an unknown reason.
-pub struct UnableToReadConfigError {
-    pub message: String,
-}
-
-impl fmt::Display for UnableToReadConfigError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl UnableToReadConfigError {
-    pub fn abort(&self, exit_code: i32) -> ! {
-        println!("{} (Error code: {})", self, exit_code);
-        process::exit(exit_code)
-    }
-}
-
-/// This error is raised when the configuration file is not found.
-///
-/// This error is unrecoverable.
-pub struct NoConfigFileError {
-    pub message: String,
-}
-
-impl fmt::Display for NoConfigFileError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl NoConfigFileError {
-    pub fn abort(&self, exit_code: i32) -> ! {
-        println!("{} (Error code: {})", self, exit_code);
-        process::exit(exit_code)
-    }
-}
-
-/// If the config file does not have valid syntax, the program mus abort and prompt
-/// the user to fix it.
-pub struct InvalidConfigFileError {
-    pub message: String,
-}
-
-impl fmt::Display for InvalidConfigFileError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl InvalidConfigFileError {
-    pub fn abort(&self, exit_code: i32) -> ! {
-        println!("{} (Error code: {})", self, exit_code);
-        process::exit(exit_code)
-    }
-}
-
-/// If the config file is empty or contains empty fields, and no CLI parameters are given, this error shall be raised.
-pub struct NoConfigError {
-    pub message: String,
-}
-
-impl fmt::Display for NoConfigError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl NoConfigError {
-    pub fn abort(&self, exit_code: i32) -> ! {
-        println!("{} (Error code: {})", self, exit_code);
-        process::exit(exit_code)
     }
 }
